78e9f83a721582a6aeafcc92aaabdc81d00bd523
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-03 21:57:45 -0700

    prints on uppercase and lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main block
+ * Description: prints alphabets in lowercase and uppercase
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; char = 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints alphabets in lowercase and uppercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
