a52db585a21f9c8e0aa8f8c304946dbd4bff741c
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-03 22:31:57 -0700

    lowercase letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints lowercase letters except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		if (letters != 'e' &&letters != 'q')
+			putchar(letters);
+	}
+	putchar('\n');
+
+	return (0);
+}
