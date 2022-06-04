f1887cc1ccf5b5501be714c00786d898e0064758
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-03 22:46:43 -0700

    prints single digit letters to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints single digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+
+
+}
