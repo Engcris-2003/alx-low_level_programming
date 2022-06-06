afc94e1ba87800712425c45d4c11cccebdf975dc
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-06 05:13:30 -0700

    prints words exactly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" -  Dora Korpar, 2015-10-19\n";
 
-write(1, str1, 59);
+write(1, "and that piece of art is userful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
