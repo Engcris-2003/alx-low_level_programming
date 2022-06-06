88e7847cf7863e4dc068746a5f8f3f48e2941ef9
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-06 05:58:03 -0700

    prints all combinations of two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 	for (num2 = num1 + 1; num2 <= 99; num2++)
-	}
-	putchar(num1 / 10) + '0');
-	putchar(num1 % 10) + '0');
+	{
+	putchar((num1 / 10) + '0');
+	putchar((num1 % 10) + '0');
 	putchar(' ');
 	putchar((num2 / 10) + '0');
 	putchar((num2 % 10) + '0');
