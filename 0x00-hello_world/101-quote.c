25ca1cb52d51ae7e8b0d4f77e0c1a21e27f56ef5
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-06 00:16:37 -0700

    Prints exactly the words to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\n" -  Dora Korpar, 2015-10-19\n" ,59);
+char str1[] = "and that piece of art is useful\" -  Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
 return (1);
 }
