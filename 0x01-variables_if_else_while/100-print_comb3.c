de6abf63c1c595c014e3dddb2fcb39ade8869673
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-06 05:38:36 -0700

    Prints all possible solutions of two-digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * mai - main point
+ * Description: prints all combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+	for (num2 = num1 + 1; num2 <= '9'; num2++)
+	}
+	putchar(num1);
+	putchar(num2);
+
+		if (num1 == '8' && num2 == '9')
+				{
+					continue;
+				}
+	putchar(',');
+	putchar(' ');
+
+	}
+	}
+	putchar('\n');
+	return (0);
+}
