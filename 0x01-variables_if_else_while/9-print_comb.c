e196de8f787c10194a029c0d54957cb7ff7b06a2
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-06 06:43:02 -0700

    Prints single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - main point
  * Description: prints a single digit
  *
  * Return: 0
@@ -9,8 +9,12 @@ int main(void)
 {
 	int num1;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = '0'; num1 <= '9', num1++)
 		putchar(num1);
+	putchar(',');
+	putchar(' ');
+
 	putchar('\n');
 	return (0);
 }
+
