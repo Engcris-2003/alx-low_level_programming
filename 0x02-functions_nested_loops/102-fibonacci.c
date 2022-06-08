368c5b7f40284ba54e4c7e794259c48111b4fefb
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-08 12:46:56 -0700

    prints the first 50 Fibonacci numbers,

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	long int i, j, k;
+	long int i, j, k, next;
 
 	j = 1;
 	k = 2;
 
-	for (i = 1; i <= 50; ++1)
+	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
