ec192919ff81e30ef10ea650baa8ce77416baac8
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-10 05:58:05 -0700

    prints remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,7 @@ include "main.h"
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 == 0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
