619d285fc5ee84b2b656bde175ea2b73fcbeddc6
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-10 00:15:11 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void more_numbers(void);
-int _isdigit(int c);
 void print_most_numbers(void);
 #endif /* MAIN_H */
