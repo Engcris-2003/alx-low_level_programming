ebeaea3835f56457daf7c471b709770c9bdba135
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-10 02:06:06 -0700

    function that draws a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints the diagonal
+ * @n: character
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('/n');
+		}
+	}
+}
