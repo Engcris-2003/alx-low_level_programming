6a6b067f81b9590fd4592a9001705f26a5e9e02d
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-10 00:28:00 -0700

    prints numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <=14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			_putchar(j + '0');
 		}
 		_putchar(i);
 	}
+	_putchar('\n');
 }
