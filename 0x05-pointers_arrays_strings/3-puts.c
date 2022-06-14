e7543ada4b43dc7943760207fef1cf2d0c3df6b5
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-14 02:12:57 -0700

    prints the string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts -  prints a string
+ * @str: character
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
+
+
