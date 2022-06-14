40d3f0abba9a276fe456c69c1bc775823b28cc96
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-14 04:33:07 -0700

    prints character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str: character
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
