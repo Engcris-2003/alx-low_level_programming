7e617dde6bfb61ac697d29f42d43fce8af46aa84
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-17 00:22:46 -0700

    capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *x)
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || x[a - 1] == spc[i] && x[a] >= 97 && x[a] <= 122))
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
 				x[a] = x[a] - 32;
 			i++;
 		}
