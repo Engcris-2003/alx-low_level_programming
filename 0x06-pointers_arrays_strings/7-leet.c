dfb4b4bbdb9b37f4b322413b59363c3a06bd335a
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-17 01:08:35 -0700

    encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string
+ * @x: string parameter
+ * Return: string
+ */
+char *leet(char *x)
+{
+	int a, b, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
+
+	while (x[a])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+				x[a] = trw[b];
+			b++;
+		}
+		a++;
+	}
+	return (x);
+}
