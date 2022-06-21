afd0625d7dea045ebc841ca695819fb6ebc0a042
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-21 00:32:33 -0700

    string character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,6 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *strchr(const char *s, int c);
+	int i = 0, j;
 
+	while (s[i])
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		if (c == s[j])
+			s += j;
+			return (s);
+	}
+	return ('\0');
 }
