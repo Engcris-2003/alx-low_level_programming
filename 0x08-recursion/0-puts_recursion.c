d2c2c5e06dda0813ce72d8b364f99966196f01c7
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-22 11:53:56 -0700

    print string and newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string followed by newline
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(**s);
+	}
+}
