4281c5baef4963a3cc6a94d171391b85bf0be5b5
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-24 07:54:46 -0700

    prints single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,15 @@ int main(void)
 {
 	int num1;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = 48 num1 < 58; num1++)
+	{
 		putchar(num1);
-	putchar(',');
-	putchar(' ');
+		if (num1 != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
 	putchar('\n');
 	return (0);
