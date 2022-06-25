160c55dea2cb1e7cb3b067cc0ec597af537a61d7
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-24 21:24:17 -0700

    second argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main point
+ * Description: prints all auguments it receives
+ * @argv: array
+ * @argc: argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("argv index %d is %c\n", a, argv[a]);
+		return (0);
+	}
+}
