70bc225b5ba2c2d73a952ab222fc2dfb61f9e388
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-24 20:38:39 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main point
+ * Description: prints it's name
+ * @argv: argument vector
+ * @argc: argument
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is argc and argv project
