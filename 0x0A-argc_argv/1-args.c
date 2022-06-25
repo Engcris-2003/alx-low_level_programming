db1b0ed11c4ed514d68947887b81b03e0f450e80
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-24 21:08:49 -0700

    first argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main point
+ * Description: prints nummber of auguments passed
+ * @argv: argument vector
+ * @argc: integer
+ * Return: 0
+ */
+int main(int argc, char argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
