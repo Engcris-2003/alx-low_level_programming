67993cfe2d086ccde15cf0d273100c0a40c023b0
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-27 07:29:33 -0700

    static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-int _islower(char c);
-int _isalpha(char c);
-int _abs(char c);
-int _isupper(char c);
-int _isdigit(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
