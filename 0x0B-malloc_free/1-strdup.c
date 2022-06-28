0a825f6b0e133d14395fd73edc6b263463049822
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-28 05:47:55 -0700

    standard up

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strdup - gives out a string with a space
+ * @str: string
+ * Return: NULL
+ */
+char *_strdup(char *str)
+{
+	int i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
