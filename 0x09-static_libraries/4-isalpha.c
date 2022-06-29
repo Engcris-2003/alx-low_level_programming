e5e723acbd924a7c854e0006d02669042da2a861
Engcris-2003 <atiochristabel639@gmail.com>
2022-06-29 03:44:28 -0700

    hot fix

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - writes a function that checks lowercase character
+ * _isalpha - writes a function that checks alphabet character
  * @c: a character argument
  * Return: returns 0 or 1 depending on condition
  */
-int _islower(int c)
+int _isalpha(int c);
 {
 	return (c >= 'a' && c <= 'z');
 }
