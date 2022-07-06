f0341c7584e984337be4d8042b2290e868526a35
Engcris-2003 <atiochristabel639@gmail.com>
2022-07-05 19:25:13 -0700

    structure dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is srtuctures and typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+/**
+ * struct dog - structure dog
+ * @name: structure dog member
+ * @age: structure dog member
+ * @owner: structure dog member
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+typedef struct dog dog_t;
+#endif
