811ef44f8a360f504731dabb9a2d2ecccad4a499
Engcris-2003 <atiochristabel639@gmail.com>
2022-07-29 19:58:38 -0700

    copy file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * error_file -checks if the files can be opened
+ * @file_to: file to
+ * @file_from: file from
+ * @argv: argument vector
+ * Return: nothing
+ */
+void error_file(int file_to, int file_from, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+/**
+ * main - checks the code
+ * @argc: number of auguments
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err;
+	ssize_t i, j;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	err(file_from, file_to, argv);
+
+	i = 1024;
+	while (i == 1024)
+	{
+		i = read(file_from, buffer, 1024);
+		if (i == -1)
+			err(-1, 0, argv);
+		j = write(file_to, buffer, i);
+		if (j == -1)
+			err(0, -1, argv);
+	}
+	err = close(file_from);
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	err = close(file_to);
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
